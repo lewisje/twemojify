477ee3ccc4e10feafbf1886938eccb303e061723
James Edward Lewis II <lewisje@users.noreply.github.com>
2015-03-29 05:55:47 -0400

    https://github.com/twitter/twemoji/issues/55

--- extension/twemojify.js
+++ extension/twemojify.js
@@ -1,1 +1,11 @@
-twemoji.parse(document.body, {size:"16x16"});
+(function twemojify(window, undefined) {
+  'use strict';
+  var r;
+  function init(e) {
+    if (e) document.removeEventListener(e.type, init, false);
+    twemoji.parse(document.body, {size: '16x16'});
+  }
+  r = document.readyState;
+  if (r === 'complete' || r === 'loaded' || r === 'interactive') init();
+  else document.addEventListener('DOMContentLoaded', init, false);
+}(window));
